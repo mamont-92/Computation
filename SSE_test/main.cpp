100b3a6409ba201440c84502dcb53aec26b9278e
fedor <gigaslon@yandex.ru>
2018-01-22 23:29:07 +0300

    remove user file

--- SSE_test/main.cpp
+++ SSE_test/main.cpp
@@ -1,8 +1,12 @@
 #include <QCoreApplication>
+#include <chrono>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "hello SSE";
 
     return a.exec();
 }
